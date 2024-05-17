d54349c20b6255db0a102334c1747514f7c1dabe
Joyce <160253116+Joyce-Gatura@users.noreply.github.com>
2024-05-17 21:10:38 +0300

    availlableFood.js

--- javascriptExercises/Array.js/availlableFood.js
+++ javascriptExercises/Array.js/availlableFood.js
@@ -1,3 +1,7 @@
+
+
+
+
 const availableFoods = [
     {id: "qwe234dfh", name: "Burger", image:"🍔🍔", price: 234},
     {id: "qwe2356dxh", name: "pizza", image:"🍕🍕", price: 400},
@@ -17,3 +21,18 @@ console.log(filteredPrices)
 
 let totalprice=filteredPrices.map(filteredfoodobj=>filteredfoodobj.price).reduce((prev,next)=>prev+next)
 console.log( `My total bill for items above 450 is :${totalprice}`)
+
+
+
+
+//Outputs:
+
+//[
+//  { id: 'qwe2456yh', name: 'meat', image: '🍖🍖', price: 500 },
+//  { id: 'qwe2785yh', name: 'chicken', image: '🍗🍗', price: 1200 }
+]
+//My total bill for items above 450 is :1700
+
+
+
+
